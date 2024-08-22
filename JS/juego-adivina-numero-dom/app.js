836a83a9c3afcc9bec025afc1070deb8443899a0
Gabriela Rojas <grojas9807@gmail.com>
2024-08-21 23:55:37 -0500

    Add validation no repeat random numbers

--- JS/juego-adivina-numero-dom/app.js
+++ JS/juego-adivina-numero-dom/app.js
@@ -1,5 +1,8 @@
 let numeroSecreto = 0;
 let intentos = 0;
+// Para garantizar que los números aleatorios no se repitan
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 function asignarTextoElemento(elemento, texto){
   let elementoHTML = document.querySelector(elemento);
@@ -31,7 +34,22 @@ function cleanBox() {
 }
 
 function generateRandomNumber(){
-  return Math.floor(Math.random()*10)+1;
+  let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+  console.log(numeroGenerado);
+  console.log(listaNumerosSorteados);
+
+  //Si ya sorteamos todos los números
+  if (listaNumerosSorteados.length == numeroMaximo) {
+    asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+} else {
+    //Si el numero generado está incluido en la lista 
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto();
+    } else {
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
+}
 }
 
 function initialConditions() {
